970446ed49bba36a3c1b07655d1768e3c2b7f40d
Shihanhussain <shihanktmrc200@gmail.com>
2023-07-11 07:44:27 +0530

    New file added

---
+++ src/Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+
+    public static void main(String[] args) {
+        int number = 5;
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    private static long calculateFactorial(int number) {
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
